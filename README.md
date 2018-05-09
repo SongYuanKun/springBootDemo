bfbce1bc9f650a8e7fc9dce1b7a65fd5eaa6390c
songyuankun/songyuankun <fhxygxXHjih67TaK>
2018-05-09 10:03:21 +0800

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # springBootDemo
+
+##注解说明
+
+###@EnableAutoConfiguration 自动加载配置文件
+
+###@ComponentScan   自动扫描注解
+
+###@SpringBootApplication   springBoot主程序入口
+
+###@Configuration   配置注解
+
+###@Value("${mongo.repset}")   读取配置文件中配置的值
+
+###@Document  mongodb集合名称
+
+###@Id  实体主键
+
+###@RestController  restful风格的接口，返回数据只能是String
+
+###@PropertySource  读取配置文件
+
+###@Bean    注解实例
+
+
+
+
+##配置文件加载顺序
+这里说了四种方式可以把配置文件放到外部的。 
+第一种是在jar包的同一目录下建一个config文件夹，然后把配置文件放到这个文件夹下； 
+第二种是直接把配置文件放到jar包的同级目录； 
+第三种在classpath下建一个config文件夹，然后把配置文件放进去； 
+第四种是在classpath下直接放配置文件。
+
+这四种方式的优先级是从一到四依次降低的。
+
--- src/main/java/com/songyuankun/mongo/MongoJdbc.java
+++ src/main/java/com/songyuankun/mongo/MongoJdbc.java
@@ -1,88 +1,90 @@
-//package com.songyuankun.mongo;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//
-//import com.mongodb.MongoClient;
-//import com.mongodb.MongoClientOptions;
-//import com.mongodb.MongoCredential;
-//import com.mongodb.ServerAddress;
-//import com.mongodb.client.MongoDatabase;
-///**
-// * @author Administrator
-// */
-//@Configuration
-//public class MongoJdbc {
-//	/** 线程安全的数据库链接 */
-//	private static MongoDatabase mongoDatabase = null;
-//	/** 服务器信息 */
-//	@Value("${mongo.repset}")
-//	private String repset;
-//	/** 数据库信息 */
-//	@Value("${mongo.database}")
-//	private String database;
-//	/** 用户名 */
-//	@Value("${mongo.user}")
-//	private String user;
-//	/** 密码 */
-//	@Value("${mongo.password}")
-//	private String password;
-//	/** 单个host允许链接的最大链接数 */
-//	@Value("${mongo.connectionsPerHost}")
-//	private String connectionsPerHost;
-//	/** 线程队列数 */
-//	@Value("${mongo.threadsAllowedToBlockForConnectionMultiplier}")
-//	private String threadsAllowedToBlockForConnectionMultiplier;
-//
-//
-//	private void init() {
-//		List<ServerAddress> serverAddressList = new ArrayList<ServerAddress>();
-//		if (this.repset == null || "".equals(this.repset)) {
-//			throw new MongodbException("未配置ip地址和端口号！");
-//		}
-//		if (this.connectionsPerHost == null || "".equals(this.connectionsPerHost)) {
-//			throw new MongodbException("未配置单个host允许链接的最大链接数！");
-//		}
-//		if (this.threadsAllowedToBlockForConnectionMultiplier == null || "".equals(this.threadsAllowedToBlockForConnectionMultiplier)) {
-//			throw new MongodbException("未配置线程队列数！");
-//		}
-//		if (this.user == null || "".equals(this.user)) {
-//			throw new MongodbException("未配置用户名！");
-//		}
-//		if (this.password == null || "".equals(this.password)) {
-//			throw new MongodbException("未配置密码！");
-//		}
-//		if (this.database == null || "".equals(this.database)) {
-//			throw new MongodbException("未配置库名！");
-//		}
-//		String[] hostPorts = this.repset.split(",");
-//		for (String hostPort : hostPorts) {
-//			String[] hostPortArr = hostPort.split(":");
-//			if (hostPortArr.length != 2) {
-//				throw new MongodbException("mongodb主机配置异常");
-//			}
-//			String host = hostPortArr[0];
-//			String port = hostPortArr[1];
-//			ServerAddress serverAddress = new ServerAddress(host, Integer.parseInt(port));
-//			serverAddressList.add(serverAddress);
-//		}
-//		MongoClientOptions mongoClientOptions = new MongoClientOptions.Builder().connectionsPerHost(Integer.parseInt(this.connectionsPerHost))
-//				.threadsAllowedToBlockForConnectionMultiplier(Integer.parseInt(this.threadsAllowedToBlockForConnectionMultiplier)).build();
-//		MongoCredential credential = MongoCredential.createCredential(this.user, this.database, this.password.toCharArray());
-//		/* 客户端连接池 */ /** 客户端连接池 */
-//		MongoClient mongoClient = new MongoClient(serverAddressList, Arrays.asList(credential), mongoClientOptions);
-//		mongoDatabase = mongoClient.getDatabase(database);
-//	}
-//
-//	public MongoDatabase getMongodb() {
-//		if (null == mongoDatabase) {
-//			System.out.println("加载mongo");
-//			init();
-//		}
-//		return mongoDatabase;
-//	}
-//}
+package com.songyuankun.mongo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoDatabase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Administrator
+ */
+@Configuration
+@PropertySource(value = "classpath*:mongodb.properties")
+public class MongoJdbc {
+    @Value("${mongo.repset}")
+    private String repset;
+    /**
+     * 数据库信息
+     */
+    @Value("${mongo.database}")
+    private String database;
+    /**
+     * 用户名
+     */
+    @Value("${mongo.user}")
+    private String user;
+    /**
+     * 密码
+     */
+    @Value("${mongo.password}")
+    private String password;
+    /**
+     * 单个host允许链接的最大链接数
+     */
+    @Value("${mongo.connectionsPerHost}")
+    private String connectionsPerHost;
+    /**
+     * 线程队列数
+     */
+    @Value("${mongo.threadsAllowedToBlockForConnectionMultiplier}")
+    private String threadsAllowedToBlockForConnectionMultiplier;
+
+    @Bean
+    public MongoDatabase mongoDatabase() {
+        List<ServerAddress> serverAddressList = new ArrayList<ServerAddress>();
+        if (this.repset == null || "".equals(this.repset)) {
+            throw new MongodbException("未配置ip地址和端口号！");
+        }
+        if (this.connectionsPerHost == null || "".equals(this.connectionsPerHost)) {
+            throw new MongodbException("未配置单个host允许链接的最大链接数！");
+        }
+        if (this.threadsAllowedToBlockForConnectionMultiplier == null || "".equals(this.threadsAllowedToBlockForConnectionMultiplier)) {
+            throw new MongodbException("未配置线程队列数！");
+        }
+        if (this.user == null || "".equals(this.user)) {
+            throw new MongodbException("未配置用户名！");
+        }
+        if (this.password == null || "".equals(this.password)) {
+            throw new MongodbException("未配置密码！");
+        }
+        if (this.database == null || "".equals(this.database)) {
+            throw new MongodbException("未配置库名！");
+        }
+        String[] hostPorts = this.repset.split(",");
+        for (String hostPort : hostPorts) {
+            String[] hostPortArr = hostPort.split(":");
+            if (hostPortArr.length != 2) {
+                throw new MongodbException("mongodb主机配置异常");
+            }
+            String host = hostPortArr[0];
+            String port = hostPortArr[1];
+            ServerAddress serverAddress = new ServerAddress(host, Integer.parseInt(port));
+            serverAddressList.add(serverAddress);
+        }
+        MongoClientOptions mongoClientOptions = new MongoClientOptions.Builder().connectionsPerHost(Integer.parseInt(this.connectionsPerHost))
+                .threadsAllowedToBlockForConnectionMultiplier(Integer.parseInt(this.threadsAllowedToBlockForConnectionMultiplier)).build();
+        MongoCredential credential = MongoCredential.createCredential(this.user, this.database, this.password.toCharArray());
+        /* 客户端连接池 */
+        MongoClient mongoClient = new MongoClient(serverAddressList, Collections.singletonList(credential), mongoClientOptions);
+        return mongoClient.getDatabase(database);
+    }
+}
