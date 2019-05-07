1b54698851897661a21243794d60fecd86435e5f
songyuankun <songyuankun@huohua.cn>
2019-05-07 16:00:27 +0800

    修改地址

--- src/main/java/com/songyuankun/MySpringBootApplication.java
+++ src/main/java/com/songyuankun/MySpringBootApplication.java
@@ -2,11 +2,13 @@ package com.songyuankun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Hello world
@@ -30,5 +32,11 @@ public class MySpringBootApplication {
     @RequestMapping("/hello")
     public String helloWord() {
         return "Hello World!";
+    }
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
